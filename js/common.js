092ca11c53f8e40f00801f9be8d2d35abab7b630
Unknown <emrdl7@gmail.com>
2018-07-04 18:09:20 +0900

    경험공유, 마이페이지 추가, 서브이미지영역 변경

--- js/common.js
+++ js/common.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
     $('.main-issue .counter>ul').slick({
         speed: 1000,
@@ -10,45 +10,91 @@ $(document).ready(function() {
         fade: true,
         cssEase: 'linear',
         respondTo: 'slider'
-      });
+    });
 
-      
-    $('.main-issue .weather>ul').slick({
-        speed: 1000,
-        autoplay: true,
-        vertical: true,
-        arrows: false
-      });
-      
+    $('.main-issue .weather>ul').slick(
+        {speed: 1000, autoplay: true, vertical: true, arrows: false}
+    );
 
-      try {
-          
-        $.datepicker.setDefaults({
-            dateFormat: 'yy-mm-dd',
-            prevText: '이전 달',
-            nextText: '다음 달',
-            monthNames: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
-            monthNamesShort: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
-            dayNames: ['일', '월', '화', '수', '목', '금', '토'],
-            dayNamesShort: ['일', '월', '화', '수', '목', '금', '토'],
-            dayNamesMin: ['일', '월', '화', '수', '목', '금', '토'],
-            showMonthAfterYear: true,
-            yearSuffix: '년'
-        });
+    try {
 
+        $
+            .datepicker
+            .setDefaults({
+                dateFormat: 'yy-mm-dd',
+                prevText: '이전 달',
+                nextText: '다음 달',
+                monthNames: [
+                    '1월',
+                    '2월',
+                    '3월',
+                    '4월',
+                    '5월',
+                    '6월',
+                    '7월',
+                    '8월',
+                    '9월',
+                    '10월',
+                    '11월',
+                    '12월'
+                ],
+                monthNamesShort: [
+                    '1월',
+                    '2월',
+                    '3월',
+                    '4월',
+                    '5월',
+                    '6월',
+                    '7월',
+                    '8월',
+                    '9월',
+                    '10월',
+                    '11월',
+                    '12월'
+                ],
+                dayNames: [
+                    '일',
+                    '월',
+                    '화',
+                    '수',
+                    '목',
+                    '금',
+                    '토'
+                ],
+                dayNamesShort: [
+                    '일',
+                    '월',
+                    '화',
+                    '수',
+                    '목',
+                    '금',
+                    '토'
+                ],
+                dayNamesMin: [
+                    '일',
+                    '월',
+                    '화',
+                    '수',
+                    '목',
+                    '금',
+                    '토'
+                ],
+                showMonthAfterYear: true,
+                yearSuffix: '년'
+            });
 
-      
         $("#day-start").datepicker();
         $("#day-end").datepicker();
+        $("#date").datepicker();
 
-    } catch(e){};
+    } catch (e) {};
 
+    $("section.sub-image").css(
+        "background",
+        "url('"+ $(this).find("img").attr("src") +"') no-repeat center / cover"
+    )
 
-
-
-
-
-      $('.sub-view-head .image ul').slick({
+    $('.sub-view-head .image ul').slick({
         speed: 1000,
         autoplay: false,
         nextArrow: ".image .next",
@@ -56,80 +102,75 @@ $(document).ready(function() {
         dots: true,
         dotsClass: 'custom-paging',
         customPaging: function (slider, i) {
-            //FYI just have a look at the object to find available information
-            //press f12 to access the console in most browsers
-            //you could also debug or look in the source
+            // FYI just have a look at the object to find available information press f12 to
+            // access the console in most browsers you could also debug or look in the
+            // source
             console.log(slider);
-            return  (i + 1) + '/' + slider.slideCount;
+            return (i + 1) + '/' + slider.slideCount;
         }
-      });
+    });
 
-      
-      function modeChange() {
+    function modeChange() {
 
         var windowWidth = $(window).width();
         if (windowWidth < 1199) {
 
-            $('.main-best .best>ul').slick({
-                speed: 1000,
-                autoplay: true,
-                vertical: true,
-                arrows: false,
-                
-            });
+            $('.main-best .best>ul').slick(
+                {speed: 1000, autoplay: true, vertical: true, arrows: false}
+            );
 
-
-            $('#header .menu h2 a').bind("click", function(e) {
+            $('#header .menu h2 a').bind("click", function (e) {
                 e.preventDefault();
-                $(this).parent().parent().toggleClass("on");
+                $(this)
+                    .parent()
+                    .parent()
+                    .toggleClass("on");
             });
-        
 
-        }
-        else {
+        } else {
             try {
                 $('.main-best .best>ul').slick('unslick');
-            } catch(e){};
+            } catch (e) {};
 
-
-            $('#header .menu li').hover(function() {
+            $('#header .menu li').hover(function () {
                 $(this).addClass("on");
-            }, function(){
+            }, function () {
                 $(this).removeClass("on")
             });
 
-            $('#header .menu h2 a').unbind("click", function(e) {
+            $('#header .menu h2 a').unbind("click", function (e) {
                 e.preventDefault();
-                $(this).parent().parent().toggleClass("on");
+                $(this)
+                    .parent()
+                    .parent()
+                    .toggleClass("on");
             });
-        
-
 
         }
     }
     modeChange();
 
-    
-    $(window).resize(function() {
+    $(window).resize(function () {
         try {
             modeChange();
-        } catch(e){};
+        } catch (e) {};
     });
 
-
-
-    $('.mobile-menu').on('click', function() {
+    $('.mobile-menu').on('click', function () {
         $('nav#main-menu').toggleClass("on");
     });
 
-    $('aside.sub-navigation>div>ul>li>a').on('click', function() {
+    $('aside.sub-navigation>div>ul>li>a').on('click', function () {
         $(this).toggleClass("on");
-        $(this).find("i").toggleClass("on");
+        $(this)
+            .find("i")
+            .toggleClass("on");
     });
 
-    $('.orum-search button.more').on('click', function() {
+    $('.orum-search button.more').on('click', function () {
         $(".orum-search ul").toggleClass("on");
-        $(this).find("i").toggleClass("on");
+        $(this)
+            .find("i")
+            .toggleClass("on");
     });
 });
-
